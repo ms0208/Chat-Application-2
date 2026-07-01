b4c026124d33cb59525b8571d9cc57c267d97db0
ms0208 <manav.8230680@gmail.com>
2026-07-02 00:00:05 +0530

    first commit

---
+++ backend/lib/utils.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const JWT = process.env.JWT_SECRET
+  const token = jwt.sign({ userId }, JWT, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+  });
+
+  return token;
+};
